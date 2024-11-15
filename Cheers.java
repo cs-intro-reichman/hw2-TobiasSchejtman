4e9ed80886523172313014735d7f45d860a24c19
TobiasSchejtman <tobias.schejtman@post.runi.ac.il>
2024-11-15 19:14:53 +0200

    exercise 3 finished

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,43 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                String name = args[0];
+                int number = Integer.parseInt(args[1]);
+                String nameUpperCase = name.toUpperCase();
+
+                int length = nameUpperCase.length();
+                String letters = "AEFHILMNORSX";
+
+                for (int i = 0; i < length; i++) {
+
+                        char currentChar = nameUpperCase.charAt(i);
+
+                        if (letters.indexOf(currentChar) == -1) {
+                                System.out.println("Give me a " + currentChar + ": " + currentChar + "!");
+                            } else {
+                                System.out.println("Give me an " + currentChar + ": " + currentChar + "!");
+                            }
+
+                }
+
+                System.out.println("What does that spell?");
+
+                for (int i = 0; i < number; i++) {
+                        
+                        System.out.println(nameUpperCase + "!!!");
+                }
+
+
+
+
+
+
+
+
+                
+                
+
+
         }
 }
