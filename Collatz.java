95e0c89c727c36556525e9ca632f7a7fff64a76c
TobiasSchejtman <tobias.schejtman@post.runi.ac.il>
2024-11-18 16:39:54 +0200

    look how much i have correct

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,38 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		
+
+		String mode = args[1];
+		
+		
+		if (mode.equals("v")) {
+
+			for (int i = 1; i <= seed; i++) { 
+		
+				String sequence = "";
+				int number = i;
+				sequence += number;
+
+				while (number > 1) {
+			
+					if (number % 2 == 0) {
+						number /= 2;
+					} else {
+						number = number * 3 + 1;
+					}
+
+					sequence += ", " + number;
+
+				}
+
+				System.out.println(sequence);
+			}
+
+			} else {
+				System.out.println("error");
+			}
+		}
 	}
-}
+
